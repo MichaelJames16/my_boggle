3a30ab1bf58ebe749146fe1b513e7f9c1a55c689
MichaelJames16 <m.harrelson86@gmail.com>
2018-07-23 09:06:08 -0600

    Added a TODO list and once more just making sure commits are working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#mazeForm').on('submit', function(event) {
 		
-		//var userInput = $('#mazeEntry').val();
+		var userInput = $('#mazeEntry').val();
 		
         var answer = 'Word: "", Score: '; 
         $('#mazeSolution').html(answer);
