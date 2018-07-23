236d0e3011274d222f1e03584f6d7f172852759b
MichaelJames16 <m.harrelson86@gmail.com>
2018-07-23 09:33:06 -0600

    Wrote the word scoring function and did some simple testing on it.

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 $(function() {
     $('#mazeForm').on('submit', function(event) {
 		
+		// Get user input
 		var userInput = $('#mazeEntry').val();
 		
+		// Score words that are found
+		var score = scoreWord("yayers");
+		
         var answer = 'Word: "", Score: '; 
-        $('#mazeSolution').html(answer);
+        $('#mazeSolution').html(score);
         event.preventDefault();
     });
+	
+	// Function that walks character by character and scores words
+	function scoreWord(word){
+		
+		// Initial score
+		var score = 0;
+		
+		// Iterate through word and score each letter
+		for(var i = 0; i < word.length; i++){
+			// Check for 'y'
+			if (word.charAt(i) == 'y'){
+				score -= 10;
+			}
+			// Check for the real vowels
+			else if(word.charAt(i) == 'a' ||
+					word.charAt(i) == 'e' ||
+					word.charAt(i) == 'i' ||
+					word.charAt(i) == 'o' ||
+					word.charAt(i) == 'u'){
+						score += 3;
+					}
+			else{
+				score -= 2;
+			}
+		}
+		
+		return score;
+	}
+
+/* TODO
+- write a word scoring function
+- write recursive board navigation
+*/
 });
+
+
